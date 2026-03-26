9b19331d19f87e6a063f46f7be624b45f3e965bc
Omar Abi Farraj <omar.7tech@gmail.com>
2026-03-26 13:47:39 +0200

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append([
             ForceJsonResponse::class,
-            RequiresJson::class,
-            
+            RequiresJson::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
